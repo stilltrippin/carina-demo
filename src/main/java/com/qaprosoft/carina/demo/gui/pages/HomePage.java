93f464e6decad43a05b593db35ceb67489ee7d3a
Oleh Sliusar <olehsliusar@Olehs-MacBook-Air.local>
2022-03-23 16:26:50 +0200

    Login success and login fail checked

--- src/main/java/com/qaprosoft/carina/demo/gui/pages/HomePage.java
+++ src/main/java/com/qaprosoft/carina/demo/gui/pages/HomePage.java
@@ -1,19 +1,19 @@
 /*
- * Copyright 2013-2021 QAPROSOFT (http://qaprosoft.com/).
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package com.qaprosoft.carina.demo.gui.pages;
+        * Copyright 2013-2021 QAPROSOFT (http://qaprosoft.com/).
+        *
+        * Licensed under the Apache License, Version 2.0 (the "License");
+        * you may not use this file except in compliance with the License.
+        * You may obtain a copy of the License at
+        *
+        * http://www.apache.org/licenses/LICENSE-2.0
+        *
+        * Unless required by applicable law or agreed to in writing, software
+        * distributed under the License is distributed on an "AS IS" BASIS,
+        * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        * See the License for the specific language governing permissions and
+        * limitations under the License.
+        */
+        package com.qaprosoft.carina.demo.gui.pages;
 
 import java.lang.invoke.MethodHandles;
 import java.util.List;
@@ -21,6 +21,7 @@ import java.util.List;
 import com.qaprosoft.carina.core.foundation.utils.Configuration;
 import com.qaprosoft.carina.core.foundation.utils.R;
 import com.qaprosoft.carina.demo.gui.components.HeaderMenu;
+import com.qaprosoft.carina.demo.gui.components.LoginWindow;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 import org.slf4j.Logger;
@@ -47,6 +48,11 @@ public class HomePage extends AbstractPage {
     @FindBy(xpath = "//div[@class='top-bar clearfix']")
     private HeaderMenu headerMenu;
 
+    @FindBy(xpath = "//span[@id='login-popup2']")
+    private LoginWindow loginWindow;
+
+
+
     public HomePage(WebDriver driver) {
         super(driver);
         setUiLoadedMarker(newsColumn);
@@ -69,12 +75,19 @@ public class HomePage extends AbstractPage {
         }
         throw new RuntimeException("Unable to open brand: " + brand);
     }
-    
+
     public WeValuePrivacyAd getWeValuePrivacyAd() {
-    	return new WeValuePrivacyAd(driver);
+        return new WeValuePrivacyAd(driver);
     }
 
     public HeaderMenu getHeaderMenu() {
         return headerMenu;
     }
+
+    public LoginWindow getLoginWindow() {
+        return loginWindow;
+    }
+
+
 }
+
